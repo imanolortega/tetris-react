7058c18c0ce59cc872581ef0d51e0217398ea63a
imanolrtega <imanolrtega@gmail.com>
2021-06-27 15:38:33 -0300

    styling, gamehelpers & tetrominos

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "./../tetrominos";
 
 const Cell = ({ type }) => {
-  return <div>Cell</div>;
+  return (
+    <StyledCell type={"L"} color={TETROMINOS["L"].color}>
+      Cell
+    </StyledCell>
+  );
 };
 
 export default Cell;
