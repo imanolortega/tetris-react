6f7eb105a5fd4729104ba0e58ee4933a0abd2dae
imanolrtega <imanolrtega@gmail.com>
2021-06-27 20:25:16 -0300

    create hooks

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { StyledStartButton } from "./styles/StyledButtonStart";
 
 const StartButton = ({ callback }) => {
-  return <div>Start Game</div>;
+  return <StyledStartButton onClick={callback}>Start Game</StyledStartButton>;
 };
 
 export default StartButton;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+//components
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
-import { createStage } from "./../gameHelpers";
+//hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+//styledComponents
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("re-render");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+          {gameOver ? (
+            <Display gameOver={gameOver} text={"Game Over"} />
+          ) : (
+            <div>
+              <Display text="Score:" />
+              <Display text="Rows:" />
+              <Display text="Level:" />
+            </div>
+          )}
           <StartButton />
         </aside>
       </StyledTetris>
