f0838c06c479e4128f2ce55b7cd5acfea9e15889
imanolrtega <imanolrtega@gmail.com>
2021-06-29 17:17:44 -0300

    rotate tetrominos

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -15,7 +15,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, playerRotate, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
@@ -60,6 +60,8 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
