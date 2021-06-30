f2ce59110a60abd8e334ac87d01343a7600a1b51
imanolrtega <imanolrtega@gmail.com>
2021-06-30 20:19:25 -0300

    fix error with linePoints

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -5,17 +5,17 @@ export const useGameStatus = (rowsCleared) => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  //each represents 1 row cleared, 2 rows cleared, 3 and 4
-  const linePoints = [40, 100, 300, 1200];
-
   const calcScore = useCallback(() => {
+    //each represents 1 row cleared, 2 rows cleared, 3 and 4
+    const linePoints = [40, 100, 300, 1200];
+
     //check if we have score
     if (rowsCleared > 0) {
       //this is how original tetris score is calculated
       setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1));
       setRows((prev) => prev + rowsCleared);
     }
-  }, [level, linePoints, rowsCleared]);
+  }, [level, rowsCleared]);
 
   useEffect(() => {
     calcScore();
